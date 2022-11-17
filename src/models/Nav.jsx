a6b8cb695adb383aa50e3a30b5c583e6765dfc27
Prikhnych <alex.prikhnych@gmail.com>
2022-11-17 11:12:03 +0200

    hello

---
+++ src/models/Nav.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Nav = () => {
+  return (
+<nav className='nav__bar'>
+  <div className="div"><a href="-#">hello</a></div>
+  <div className="div"><a href="-#">hello</a></div>
+  <div className="div"><a href="-#">hello</a></div>
+  <div className="div"><a href="-#">hello</a></div>
+  <div className="div"><a href="-#">hello</a></div>
+  <div className="div"><a href="-#">hello</a></div>
+</nav>
+  );
+};
+
+export default Nav;
